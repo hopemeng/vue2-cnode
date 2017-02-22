6637c36119aafe37edd8950f670b6edbf1a2490f
songlianmeng <837560602@qq.com>
2017-02-22 17:06:03 +0800

    个人中心

---
+++ src/pages/my/home/index.vue
@@ -0,0 +1,133 @@
+<style lang="less" scoped>
+    @import "../../../less/config";
+    .con {
+        background: #eee;
+    }
+    
+    .user {
+        position: relative;
+        height: 180px;
+        background-image: url(./headimg-bg.jpg);
+        background-position: center center;
+        background-size: cover;
+    }
+    
+    .headimg {
+        position: absolute;
+        top: 100px;
+        bottom: 0;
+        left: 50%;
+        z-index: 1;
+        width: 120px;
+        height: 120px;
+        margin: 0 0 -40px -60px;
+        border-radius: 50%;
+        border: 1px solid #ddd;
+        background-color: #eee;
+        background-size: cover;
+        background-position: center center;
+        filter: none;
+    }
+    
+    .user-name {
+        padding: 40px 0 50px;
+        line-height: 32px;
+        text-align: center;
+    }
+    
+    .nav {
+        padding: 0;
+        margin: 0;
+        li {
+            list-style: none;
+            border-bottom: 1px solid #ddd;
+            background: #fbfbfb;
+            &:first-of-type {
+                border-top: 1px solid #ddd;
+            }
+        }
+        a {
+            padding: 8px 10px;
+            line-height: 28px;
+            .icon {
+                width: 45px;
+                .iconfont {
+                    font-size: 24px;
+                    color: #aaa;
+                }
+            }
+            .text {
+                line-height: 30px;
+                font-size: 13px;
+                color: @text;
+            }
+            .icon-arrow-right {
+                color: #ccc;
+            }
+        }
+    }
+</style>
+<template>
+    <div>
+        <v-header title="个人中心">
+             
+                <div slot="right" class="item" flex="main:center cross:center" v-if="user.id" v-on:click="signout">
+                   <i style="color:#FF0033" class="iconfont icon-signout"></i>
+                </div>
+                     
+        </v-header>
+         <v-content class="con">
+             <router-link v-if="user.avatar_url" :to="'/user/' + user.loginname">
+                <div class="user"></div>
+                <div class="headimg" :style="{backgroundImage: 'url(' + user.avatar_url + ')'}" ></div>
+                <div class="user-name">{{ user.loginname }}</div>
+            </router-link>
+            <router-link v-if="!user.avatar_url" to="/login">
+                <div class="user"></div>
+                <div class="headimg"></div>
+                <div class="user-name">你还未登录，请先登录！</div>
+            </router-link>
+            <nav class="nav">
+                
+                <li>
+                    <router-link to="/my/collect" flex="box:justify">
+                        <div class="icon" flex="cross:center">
+                            <i style="color: #663300;" class="iconfont icon-collect"></i>
+                        </div>
+                        <div class="text">我的收藏</div>
+                        <div class="arrow">
+                            <i class="iconfont icon-arrow-right"></i>
+                        </div>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/about" flex="box:justify">
+                        <div class="icon" flex="cross:center">
+                            <i class="iconfont icon-about"></i>
+                        </div>
+                        <div class="text">关于</div>
+                        <div class="arrow">
+                            <i class="iconfont icon-arrow-right"></i>
+                        </div>
+                    </router-link>
+                </li>               
+            </nav>
+            <div style="height: 50px;"></div>
+         </v-content>
+         <v-footer></v-footer>
+    </div>
+</template>
+<script>
+    import { mapState, mapActions} from 'vuex'
+    import { USER_SIGNOUT } from '../../../store/user'
+    export default {
+             computed: mapState({ user: state => state.user}),
+             methods: { 
+                ...mapActions([USER_SIGNOUT]),
+                signout() {
+                    this.USER_SIGNOUT()
+                    this.$router.replace({ path: '/login'})
+                }
+             }
+    }
+</script>            
