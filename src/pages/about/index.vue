e48373a2c2195f17f7fb57b340d43c23bfe1f1a2
songlianmeng <837560602@qq.com>
2017-02-22 17:02:24 +0800

    关于

---
+++ src/pages/about/index.vue
@@ -0,0 +1,39 @@
+<style lang="less" scoped>
+    .content {
+        bottom: 0
+    }
+    .about {
+        padding: 10px;
+        line-height: 22px;
+        dt {
+            line-height: 28px;
+            font-weight: bold;
+        }
+    }
+</style>
+<template>
+    <div>
+        <v-header title="关于">
+            <div slot="left" class="item" flex="main:center cross:center" v-on:click="$router.go(-1)">
+                <i class="iconfont icon-back"></i>
+            </div>
+        </v-header>
+        <v-content class="content">
+            <div class="about">
+                <dl>
+                    <dt>前言</dt>
+                    <dd>
+                        感谢cnodejs社区提供的API。
+                    </dd>
+                    <dt>技术栈</dt>
+                    <dd>
+                        项目基于vue2、vue-resource、vue-router、vuex、flex.css、ES6进行开发，使用了vue-cli创建项目。
+                    </dd>
+                </dl>
+            </div>
+        </v-content>
+    </div>
+</template>
+<script>
+
+</script>
