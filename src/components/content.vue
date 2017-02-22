3de083f2522e3b3dae2cc567ef7aecd9acbe74e8
songlianmeng <837560602@qq.com>
2017-02-22 16:55:22 +0800

    共用组件

---
+++ src/components/content.vue
@@ -0,0 +1,21 @@
+<style lang="less" scoped>
+    @import "../less/config";
+    .content {
+        overflow: auto;
+        position: absolute;
+        top: 57px;
+        right: 0;
+        bottom: 50px;
+        left: 0;
+        z-index: 100; /*堆叠顺序*/
+        -webkit-overflow-scrolling: touch; /*快速滚动回弹*/
+    }
+</style>
+<template>
+    <div class="content">
+        <slot></slot>
+    </div>
+</template>
+<script>
+
+</script>
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import content from './content'
+import footer from './footer'
+import header from './header'
+import nodata from './data-null'
+import loading from './loading'
+export default {content, footer, header, nodata, loading}
